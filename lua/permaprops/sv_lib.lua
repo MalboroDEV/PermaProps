fb568f0cc5f6d2fc5e126fc9e1e32e37651ef473
Etos7 <31066103+Etos7@users.noreply.github.com>
2019-02-05 19:40:15 +0100

    SubMaterial Fix
    There is no continue in LUA.
    SubMaterial index starts form 0.

--- lua/permaprops/sv_lib.lua
+++ lua/permaprops/sv_lib.lua
@@ -50,7 +50,7 @@ function PermaProps.PPGetEntTable( ent )
 			if ( ent:GetSubMaterial( k )) then
 
 				content.SubMat = content.SubMat or {}
-				content.SubMat[ k ] = ent:GetSubMaterial( k )
+				content.SubMat[ k ] = ent:GetSubMaterial( k-1 )
 
 			end
 
@@ -162,9 +162,10 @@ function PermaProps.PPEntityFromTable( data, id )
 
 		for k, v in pairs( data.SubMat ) do
 
-			if type(k) != "number" or type(v) == "string" then continue end
+			if type(k) != "number" or type(v) == "string" then
 
-			ent:SetSubMaterial( k, v )
+			ent:SetSubMaterial( k-1, v )
+			end
 			
 		end
 
